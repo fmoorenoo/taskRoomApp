73fc213f0e12d8cdd61f1034a51ea9c339d94460
fmoorenoo <fermontemore@gmail.com>
2024-11-20 22:28:54 +0000

    ID del tipo de tarea asociado mediante ForeignKey y atributo 'isCompleted' reemplazado por 'description'

--- app/src/main/java/org/iesharia/taskroomapp/Task.kt
+++ app/src/main/java/org/iesharia/taskroomapp/Task.kt
@@ -2,11 +2,26 @@ package org.iesharia.taskroomapp
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.ForeignKey
 
-@Entity(tableName = "tasks")
+@Entity(
+    tableName = "tasks",
+    foreignKeys = [
+        ForeignKey(
+            entity = TaskType::class,
+            // Referencia en la tabla TaskType
+            parentColumns = ["id"],
+            // Referencia en esta tabla
+            childColumns = ["taskTypeId"],
+            // Si se borra un tipo de tarea, deben borrarse sus tareas asociadas
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class Task(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val name: String,
-    val isCompleted: Boolean = false
+    val description: String = "",
+    val taskTypeId: Int
 )
