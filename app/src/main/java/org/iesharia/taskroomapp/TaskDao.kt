8a7edef4850253d4ae65e97a8cd3d882dc848c83
fmoorenoo <fermontemore@gmail.com>
2024-11-20 22:37:21 +0000

    Métodos CRUD para la tabla 'task_type'

--- app/src/main/java/org/iesharia/taskroomapp/TaskDao.kt
+++ app/src/main/java/org/iesharia/taskroomapp/TaskDao.kt
@@ -3,9 +3,30 @@ package org.iesharia.taskroomapp
 import androidx.room.*
 
 @Dao interface TaskDao {
-    @Insert suspend fun insert(task: Task)
+// CRUD para 'Task'
+    @Insert
+    suspend fun insert(task: Task)
+
     @Query("SELECT * FROM tasks")
     suspend fun getAllTasks(): List<Task>
-    @Update suspend fun update(task: Task)
-    @Delete suspend fun delete(task: Task)
+
+    @Update
+    suspend fun update(task: Task)
+
+    @Delete
+    suspend fun delete(task: Task)
+
+
+// CRUD para 'TaskType'
+    @Insert
+    suspend fun insertTaskType(taskType: TaskType)
+
+    @Query("SELECT * FROM task_type")
+    suspend fun getAllTaskTypes(): List<TaskType>
+
+    @Update
+    suspend fun updateTaskType(taskType: TaskType)
+
+    @Delete
+    suspend fun deleteTaskType(taskType: TaskType)
 }
