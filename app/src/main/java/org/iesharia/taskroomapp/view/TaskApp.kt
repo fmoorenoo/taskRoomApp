8409c9e4c97200cdc2dc2581ede7d6d881d1a4b1
fmoorenoo <fermontemore@gmail.com>
2024-11-21 10:40:12 +0000

    Input para ingresar el ID del tipo de tarea

--- app/src/main/java/org/iesharia/taskroomapp/view/TaskApp.kt
+++ app/src/main/java/org/iesharia/taskroomapp/view/TaskApp.kt
@@ -1,5 +1,6 @@
 package org.iesharia.taskroomapp.view
 
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -10,9 +11,12 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.iesharia.taskroomapp.AppDatabase
@@ -27,6 +31,7 @@ fun TaskApp(database: AppDatabase) {
     var taskTypes by remember { mutableStateOf(listOf<TaskType>()) }
     var newTaskName by remember { mutableStateOf("") }
     var newTaskTypeName by remember { mutableStateOf("") }
+    var taskTypeID by remember { mutableStateOf("") }
     val context = LocalContext.current
 
     // Cargar tareas y tipos de tareas al iniciar
@@ -41,6 +46,18 @@ fun TaskApp(database: AppDatabase) {
             .background(Color(0xFFB6D8C1))
             .padding(16.dp)
     ) {
+        Box(modifier = Modifier.padding(vertical = 15.dp, horizontal = 5.dp)) {
+            Text(
+                text = "Lista de tareas",
+                fontWeight = FontWeight.Bold,
+                fontSize = 27.sp,
+                color = Color.White,
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(15.dp))
+                    .background(Color(0xFF598D61))
+                    .padding(horizontal = 15.dp, vertical = 7.dp)
+            )
+        }
         // Input para añadir nueva tarea
         Row(
             verticalAlignment = Alignment.CenterVertically,
@@ -50,6 +67,17 @@ fun TaskApp(database: AppDatabase) {
                 value = newTaskName,
                 onValueChange = { newTaskName = it },
                 label = { Text("Nueva tarea") },
+                modifier = Modifier.weight(3f),
+                shape = RoundedCornerShape(20.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color(0xFFCFF0D9),
+                    unfocusedContainerColor = Color(0xFFCFF0D9)
+                )
+            )
+            OutlinedTextField(
+                value = taskTypeID,
+                onValueChange = { taskTypeID = it },
+                label = { Text("ID") },
                 modifier = Modifier.weight(1f),
                 shape = RoundedCornerShape(20.dp),
                 colors = TextFieldDefaults.colors(
@@ -63,10 +91,12 @@ fun TaskApp(database: AppDatabase) {
                 onClick = {
                     if (newTaskName.isNotBlank()) {
                         scope.launch(Dispatchers.IO) {
-                            val newTask = Task(name = newTaskName, description = "d", taskTypeId = 1)
+                            val newTask = Task(name = newTaskName, description = "d", taskTypeId = taskTypeID.toInt())
                             taskDao.insert(newTask)
                             tasks = taskDao.getAllTasks()
+                            Log.i("Dam2", "$tasks")
                             newTaskName = ""
+                            taskTypeID = ""
                         }
                     } else {
                         Toast.makeText(context, "Introduce un nombre válido", Toast.LENGTH_SHORT).show()
