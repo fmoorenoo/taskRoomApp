141ebc905f5ba52d02a3afc893270208d76d26d9
fmoorenoo <fermontemore@gmail.com>
2024-11-21 08:50:33 +0000

    Directorio 'view' creado y TaskCard separado de TaskApp

--- app/src/main/java/org/iesharia/taskroomapp/view/TaskApp.kt
+++ app/src/main/java/org/iesharia/taskroomapp/view/TaskApp.kt
@@ -1,4 +1,4 @@
-package org.iesharia.taskroomapp
+package org.iesharia.taskroomapp.view
 
 import android.widget.Toast
 import androidx.compose.foundation.background
@@ -8,12 +8,7 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Create
-
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -26,6 +21,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.iesharia.taskroomapp.AppDatabase
+import org.iesharia.taskroomapp.Task
 
 @Composable
 fun TaskApp(database: AppDatabase) {
@@ -39,7 +36,6 @@ fun TaskApp(database: AppDatabase) {
     LaunchedEffect(Unit) {
         tasks = taskDao.getAllTasks()
     }
-
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -58,7 +54,6 @@ fun TaskApp(database: AppDatabase) {
                     .padding(horizontal = 15.dp, vertical = 7.dp)
             )
         }
-
 
         Row(
             verticalAlignment = Alignment.CenterVertically,
@@ -125,97 +120,6 @@ fun TaskApp(database: AppDatabase) {
                         }
                     }
                 )
-            }
-        }
-    }
-}
-
-@Composable
-fun TaskCard(task: Task, onDelete: () -> Unit, onEdit: (String) -> Unit) {
-    var name: String = task.name
-    var edit by remember { mutableStateOf(false) }
-    var editText by remember { mutableStateOf(name) }
-
-    Card(
-        modifier = Modifier
-            .fillMaxWidth(),
-        colors = CardDefaults.cardColors(
-            containerColor = Color(0xFFDAF4E2)
-        ),
-        shape = RoundedCornerShape(5.dp)
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(top = 8.dp, start = 8.dp, end = 8.dp)
-                .fillMaxWidth(),
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = "Task",
-                    fontWeight = FontWeight(500),
-                    modifier = Modifier
-                        .clip(shape = RoundedCornerShape(6.dp))
-                        .background(Color(0xFF83E4A4))
-                        .padding(horizontal = 8.dp, vertical = 4.dp)
-                )
-
-                Text(
-                    text = "ID-${task.id}",
-                    fontWeight = FontWeight(500),
-                    fontSize = 17.sp
-                )
-            }
-
-            if (edit) {
-                OutlinedTextField(
-                    value = editText,
-                    onValueChange = { editText = it },
-                    label = { Text("Task Name") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-            } else {
-                if (task.name.length > 10) {
-                    name = task.name.substring(0, 10) + "..."
-                }
-
-                Text(
-                    text = name.uppercase(),
-                    modifier = Modifier.padding(4.dp),
-                    fontSize = 17.sp
-                )
-            }
-
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.Bottom,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                if (edit) {
-                    IconButton(onClick = {
-                        onEdit(editText)
-                        edit = false
-                    }) {
-                        Icon(Icons.Default.Check, contentDescription = "Save", tint = Color(0xFF4CAF50))
-                    }
-
-                    IconButton(onClick = {
-                        edit = false
-                        editText = task.name
-                    }) {
-                        Icon(Icons.Default.Close, contentDescription = "Cancel", tint = Color(0xFFFF5722))
-                    }
-                } else {
-                    IconButton(onClick = { edit = true }) {
-                        Icon(Icons.Default.Create, contentDescription = "Edit", tint = Color(0xFF3871AB))
-                    }
-                    IconButton(onClick = onDelete) {
-                        Icon(Icons.Default.Delete, contentDescription = "Delete", tint = Color(0xFFB93E3E))
-                    }
-                }
             }
         }
     }
