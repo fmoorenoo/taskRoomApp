a597f157710d4976d73a338330004c0c1dca8b3d
fmoorenoo <fermontemore@gmail.com>
2024-11-26 00:21:45 +0000

    Etiqueta 'tipo' añadida y colores modificados

--- app/src/main/java/org/iesharia/taskroomapp/view/TaskTypeCard.kt
+++ app/src/main/java/org/iesharia/taskroomapp/view/TaskTypeCard.kt
@@ -1,5 +1,6 @@
 package org.iesharia.taskroomapp.view
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -17,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -35,9 +37,9 @@ fun TaskTypeCard(task_type: TaskType, onDelete: () -> Unit, onEdit: (String) -> 
             .clickable { onSelect() }
             .border(width = 2.dp,
                 shape = RoundedCornerShape(5.dp),
-                color = if (isSelected) Color(0xFF6BCC8A) else Color.Unspecified),
+                color = if (isSelected) Color(0xFF507DE0) else Color.Unspecified),
         colors = CardDefaults.cardColors(
-            containerColor = if (isSelected) Color(0xFFB6F4CA) else Color(0xFFCFF0D9)
+            containerColor = if (isSelected) Color(0xFF82BBD9) else Color(0xFFCFF0D9)
         ),
         shape = RoundedCornerShape(5.dp)
     ) {
@@ -47,26 +49,39 @@ fun TaskTypeCard(task_type: TaskType, onDelete: () -> Unit, onEdit: (String) -> 
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(modifier = Modifier.padding(start = 8.dp)) {
+            Row(modifier = Modifier.padding(start = 8.dp).weight(2f)) {
                 if (edit) {
                     OutlinedTextField(
                         value = editText,
                         onValueChange = { editText = it },
                         label = { Text("Edit title") },
-                        modifier = Modifier.padding(bottom = 5.dp, start = 5.dp)
+                        modifier = Modifier.padding(bottom = 8.dp, start = 5.dp)
                     )
                 } else {
-                    if (task_type.title.length > 13) {
-                        name = task_type.title.substring(0, 13) + "..."
+                    if (task_type.title.length > 14) {
+                        name = task_type.title.substring(0, 14) + "..."
                     }
-                    Text(
-                        text = name,
-                        fontSize = 18.sp,
-                    )
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(
+                            text = "Tipo",
+                            fontWeight = FontWeight(500),
+                            fontSize = 17.sp,
+                            modifier = Modifier
+                                .clip(shape = RoundedCornerShape(6.dp))
+                                .background(Color(0xFFDAD3EA))
+                                .padding(horizontal = 8.dp, vertical = 4.dp)
+                        )
+                        Spacer(modifier = Modifier.width(15.dp))
+                        Text(
+                            text = name,
+                            fontSize = 18.sp,
+                        )
+                    }
+
                 }
             }
 
-            Row(horizontalArrangement = Arrangement.End) {
+            Row(horizontalArrangement = Arrangement.End, modifier = Modifier.weight(1f)) {
                 if (edit) {
                     IconButton(onClick = {
                         onEdit(editText)
